eeadbe7b05ed5123ff88a7eb4e0e609b52e283c6
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-08 23:49:07 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-echo "Running before_install script"
-
-# Example: Stop a service
-sudo systemctl stop myapp.service
-
-# Example: Remove old application files
-sudo rm -rf /var/www/app/*
-
+echo "Stopping Apache server"
+sudo systemctl stop apache2
