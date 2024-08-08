cd805ec7f31a386ee01461359f0a40c96fb8f1e0
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-08 23:49:31 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo "Running after_install script"
-
-# Example: Set file permissions
-sudo chown -R www-data:www-data /var/www/app
-
-# Example: Migrate the database
-sudo -u www-data php /var/www/app/bin/console doctrine:migrations:migrate
-
+echo "Setting permissions"
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
