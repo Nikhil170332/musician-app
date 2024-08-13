786da6fbf521775e8d3f60662d44053391aa6fd0
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-13 14:07:57 +0530

    Update stop_apache.sh

--- scripts/stop_apache.sh
+++ scripts/stop_apache.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-check = $(ps -ef | grep node | head -n 1 | awk '{print $2}')
+check = $(ps -ef | grep '\snode\s' | head -n 1 | awk '{print $2}')
 
 # Check if the Apache service stopped successfully
 if [[$check >= 0]]; then
     sudo kill -9 $check
     exit 0
 else
-    echo "It is stopped"
+    echo "It is not running"
 fi
 
