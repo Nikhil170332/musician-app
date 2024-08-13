8029265a9778c1c178194686a2ff4b363a25c5a8
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-13 14:38:18 +0530

    Update stop_apache.sh

--- scripts/stop_apache.sh
+++ scripts/stop_apache.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-check = $(ps -aux | grep app.js)
-
-# Check if the Apache service stopped successfully
-if [[$check -eq 0]]; then
-    pkill -f app.js
-    exit 0
-else
-    echo "It is not running"
-    exit 1
-fi
+pkill -f app.js
 
