0a02b2b62812cb21d77ab015d3684dff625dfb8b
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-17 12:54:53 +0530

    Update stop_apache.sh

--- scripts/stop_apache.sh
+++ scripts/stop_apache.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-node
-if echo $? -eq 127; then
+see=$(node)
+if $see -eq 127; then
     exit 0
 else
     check=$(ps -aux | grep '\snode\s' | head -n 1 | awk '{print $2}')
     ps -aux | grep '\snode\s' | head -n 1 | awk '{print $2}'
-    if echo $? -eq 0; then
+    if $check -eq 0; then
         sudo kill -9 $check
         exit 0
     else
