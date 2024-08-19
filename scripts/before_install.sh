fa36a5889bc7bdfb28ee1a1e7ddf1867b2944755
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-19 10:45:38 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,8 +2,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 cd /home/ubuntu
-sudo apt-get install -y curl
-curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
-sudo -E bash nodesource_setup.sh
-sudo apt-get install -y nodejs
-
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo npm install -g npm-install-missing -y
