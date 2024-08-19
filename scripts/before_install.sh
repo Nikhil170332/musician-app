629a3ae539611549044e0862c4b617e651ee89f8
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-19 15:01:25 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,4 +5,6 @@ cd /home/ubuntu
 sudo apt install nodejs -y
 sudo apt install npm -y
 sudo npm install -g npm-install-missing -y
-sudo npm install && npm start
+sudo rm -rf node_modules package-lock.json
+sudo npm install -y
+sudo npm start
